298a2c1fff19d5d40421ab37f24b1a203e3d4393
Ana <contact@ana.com>
2019-09-26 16:20:09 -0300

    second attempt

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,8 +6,28 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
 
+// Firebase App (the core Firebase SDK) is always required and must be listed first
+import * as firebase from "firebase/app";
+
+// Add the Firebase products that you want to use
+import "firebase/auth";
+import "firebase/firestore";
+
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD1mVAb2aaV9MT_xdSqFRcmwWWm5J4jx98",
+  authDomain: "ionic-chat-77df2.firebaseapp.com",
+  databaseURL: "https://ionic-chat-77df2.firebaseio.com",
+  projectId: "ionic-chat-77df2",
+  storageBucket: "ionic-chat-77df2.appspot.com",
+  messagingSenderId: "48089131856",
+  appId: "1:48089131856:web:bb833795dd0a5ce5f45a55"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
 @NgModule({
   declarations: [AppComponent],
---
+++ src/app/chat/chat.page.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: "app-chat",
+  templateUrl: "./chat.page.html",
+  styleUrls: ["./chat.page.scss"]
+})
+export class ChatPage implements OnInit {
+  text: string;
+  chatRef: any;
+  uid: string;
+
+  constructor() {}
+
+  send() {
+    console.log("Blep");
+  }
+
+  ngOnInit() {}
+}
